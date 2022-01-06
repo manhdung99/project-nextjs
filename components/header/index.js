89f5c3357b37f1cf640757f89b42776bb348eec5
manhdung99 <manhdung99it@gmail.com>
2022-01-06 20:18:31 +0700

    update header

--- components/header/index.js
+++ components/header/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 export default function Header() {
     return (
-        <nav className='flex justify-between items-center pt-6 pb-2 mb-6 border-b border-zinc-300 '>
+        <nav className='flex justify-between items-center pt-6 pb-2 mb-6 border-b border-zinc-400 '>
           <div>
               <Link href="/">
                   <a>Home Page</a>
